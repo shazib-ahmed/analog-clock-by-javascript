2eb6d243a846b2273af8491bc2590aac466d17c7
shazib ahmed <shajibahmed420@gmail.com>
2020-07-03 23:03:34 +0600

    analog clock by javascript

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="icon" href="Analog Clock.png" type="image/x-icon">
+	<title>Neumorphic Digital Clock via Javascript</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<div class="clock">
+		<div class="hour">
+			<div class="hr" id="hr"></div>
+		</div>
+		<div class="min">
+			<div class="mn" id="mn"></div>
+		</div>
+		<div class="sec">
+			<div class="sc" id="sc"></div>
+		</div>
+	</div>
+	<script src="main.js"></script>	
+</body>
+</html>
